f96939b803097568d86d9096248f72eb7dbad19a
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-31 20:10:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,8 @@ def happy_birthday(birthday_kids)
  if birthday_kids.each do |kids_name, age|
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 else 
-  puts ""
+  puts "You are too old for this."
+end
 end
 end
 
